e445d011cc330b89ec2f98b71de7b3fe8748b4a4
KwonMinjae <james970506@gmail.com>
2025-11-17 17:29:32 +0900

    #core-init

--- src/main/java/org/pokeherb/orderservice/infrastructure/config/FeignConfig.java
+++
@@ -1,9 +0,0 @@
-package org.pokeherb.orderservice.infrastructure.config;
-
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@EnableFeignClients("org.pokeherb.orderservice")
-public class FeignConfig {
-}
