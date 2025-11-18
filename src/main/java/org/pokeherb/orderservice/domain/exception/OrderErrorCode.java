1370dd288f9af4cdbf630b9358fea06547653d62
KwonMinjae <james970506@gmail.com>
2025-11-18 20:03:23 +0900

    feat: order- setting 완료 QueryDSL 추가 (#3)

---
+++ src/main/java/org/pokeherb/orderservice/domain/exception/OrderErrorCode.java
@@ -0,0 +1,11 @@
+package org.pokeherb.orderservice.domain.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.pokeherb.orderservice.global.infrastructure.error.BaseErrorCode;
+
+//@Getter
+//@AllArgsConstructor
+//public enum OrderErrorCode implements BaseErrorCode (
+//
+//) {}
