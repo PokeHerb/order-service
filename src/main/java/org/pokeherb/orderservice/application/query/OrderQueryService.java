0856cc8145892de9d8ddec32e0943980a5fddefd
KwonMinjae <james970506@gmail.com>
2025-11-21 10:17:31 +0900

    refactor: order query 폴더 및 파일생성

---
+++ src/main/java/org/pokeherb/orderservice/application/query/OrderQueryService.java
@@ -0,0 +1,4 @@
+package org.pokeherb.orderservice.application.query;
+
+public interface OrderQueryService {
+}
---
+++ src/main/java/org/pokeherb/orderservice/application/query/OrderQueryServiceImpl.java
@@ -0,0 +1,8 @@
+package org.pokeherb.orderservice.application.query;
+
+import org.springframework.stereotype.Service;
+
+
+public class OrderQueryServiceImpl implements OrderQueryService {
+
+}
