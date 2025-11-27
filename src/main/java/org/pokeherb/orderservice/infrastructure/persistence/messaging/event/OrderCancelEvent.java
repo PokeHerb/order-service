9c33c76cc8d8f36bc4ebe629438720ca7136f038
minjakekwon <james970506@gmail.com>
2025-11-27 14:47:03 +0900

    feat : 오더 취소 이벤트, 오더 상태 변경 이벤트(#13)

--- src/main/java/org/pokeherb/orderservice/infrastructure/persistence/messaging/event/OrderCancelEvent.java
+++ src/main/java/org/pokeherb/orderservice/infrastructure/persistence/messaging/event/OrderCancelEvent.java
@@ -5,34 +5,50 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.pokeherb.orderservice.application.command.OrderCommandService;
 import org.pokeherb.orderservice.application.service.dto.request.OrderCancelRequestDto;
+import org.pokeherb.orderservice.application.service.dto.response.OrderResponseDto;
+import org.pokeherb.orderservice.global.infrastructure.exception.CustomException;
+import org.pokeherb.orderservice.infrastructure.persistence.messaging.dto.HubOrderCanceledMessageDto;
 import org.pokeherb.orderservice.infrastructure.persistence.messaging.dto.OrderCancelMessageDto;
 import org.pokeherb.orderservice.infrastructure.persistence.messaging.messageHandler.AbstractOrderEventHandler;
 import org.pokeherb.orderservice.infrastructure.persistence.messaging.rabbit.RabbitProducer;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Slf4j
-@Component("order.canceled.order")
+@Component("hub.canceled.order")
 public class OrderCancelEvent extends AbstractOrderEventHandler {
-
-    @Autowired
-    private RabbitProducer rabbitProducer;
 
     private final OrderCommandService orderCommandService;
 
+    private final RabbitProducer rabbitProducer;
+
     public OrderCancelEvent(
             ObjectMapper objectMapper,
-            OrderCommandService orderCommandService
+            OrderCommandService orderCommandService,
+            RabbitProducer rabbitProducer
     ) {
         super(objectMapper);
         this.orderCommandService = orderCommandService;
+        this.rabbitProducer = rabbitProducer;
     }
 
     public void handle(String payload) {
-        OrderCancelMessageDto event = readPayload(payload, OrderCancelMessageDto.class);
-        OrderCancelRequestDto dto = new OrderCancelRequestDto(event.cancellerId());
-        orderCommandService.cancelOrder(event.orderId(), dto);
-        log.info("Order canceled via MQ event: orderId={}, cancellerId={}",
-                event.orderId(), event.cancellerId());
+        try {
+            OrderCancelMessageDto event = readPayload(payload, OrderCancelMessageDto.class);
+            OrderCancelRequestDto dto = new OrderCancelRequestDto(event.cancellerId());
+            OrderResponseDto response = orderCommandService.cancelOrder(event.orderId(), dto);
+            log.info("Order canceled via MQ event: orderId={}, cancellerId={}",
+                    event.orderId(), event.cancellerId());
+            HubOrderCanceledMessageDto hubEvent = new HubOrderCanceledMessageDto(
+                    event.orderId(),
+                    response.productName(),
+                    response.quantity()
+            );
+            rabbitProducer.publishDeliveryEvent(hubEvent, "hub.canceled.order");
+
+        } catch (CustomException e) {
+            log.error("Error processing OrderCancelEvent", e);
+        } catch (Exception e) {
+            log.error("Unexpected error while processing order create MQ. payload={}", payload, e);
+        }
     }
 }
--- src/main/java/org/pokeherb/orderservice/infrastructure/persistence/messaging/event/OrderStatusUpdateEvent.java
+++ src/main/java/org/pokeherb/orderservice/infrastructure/persistence/messaging/event/OrderStatusUpdateEvent.java
@@ -2,8 +2,8 @@ package org.pokeherb.orderservice.infrastructure.persistence.messaging.event;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
-import org.pokeherb.orderservice.application.command.OrderCommandService;
 import org.pokeherb.orderservice.application.service.OrderStatusUpdateService;
+import org.pokeherb.orderservice.global.infrastructure.exception.CustomException;
 import org.pokeherb.orderservice.infrastructure.persistence.messaging.dto.OrderStatusUpdateMessageDto;
 import org.pokeherb.orderservice.infrastructure.persistence.messaging.messageHandler.AbstractOrderEventHandler;
 import org.springframework.stereotype.Component;
@@ -12,20 +12,24 @@ import org.springframework.stereotype.Component;
 @Component("order.status")
 public class OrderStatusUpdateEvent extends AbstractOrderEventHandler {
 
-    private final OrderCommandService orderCommandService;
     private final OrderStatusUpdateService orderStatusUpdateService;
 
     public OrderStatusUpdateEvent(
             ObjectMapper objectMapper,
-            OrderCommandService orderCommandService,
             OrderStatusUpdateService orderStatusUpdateService) {
         super(objectMapper);
-        this.orderCommandService = orderCommandService;
         this.orderStatusUpdateService = orderStatusUpdateService;
     }
 
     public void handle(String payload) {
-        OrderStatusUpdateMessageDto event = readPayload(payload, OrderStatusUpdateMessageDto.class);
-        orderStatusUpdateService.applyStatusUpdate(event);
+        try {
+            OrderStatusUpdateMessageDto event = readPayload(payload, OrderStatusUpdateMessageDto.class);
+            orderStatusUpdateService.applyStatusUpdate(event);
+            
+        } catch (CustomException e) {
+            log.error("Error processing OrderStatusUpdateEvent", e);
+        } catch (Exception e) {
+            log.error("Unexpected error while processing order create MQ. payload={}", payload, e);
+        }
     }
 }
